59372ece766972e7577eb5a20b5550e27aa9ea8b
livelybug <livelybug@gmail.com>
2016-06-20 11:00:49 +0800

    Add value indication bar and value indicator

--- myApp.js
+++ myApp.js
@@ -1,15 +1,20 @@
 "use strict";
 
-var margin = {top: 20, right: 20, bottom: 50, left: 50},
+//var bts = document.querySelectorAll(".pEma")
+//
+//for(var i = 0; i<elems.length; i++) {
+//    elems[i].click();
+//}
+
+var margin = {top: 20, right: 20, bottom: 70, left: 90},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
 var xCol = "date"
-var yCol = "pOpen"
+var yCol = "pEma_w"
 
 var formatDate1 = d3.time.format("%d-%m-%Y");
 var formatDate2 = d3.time.format("%d/%m/%Y");
-var date_format_tick = d3.time.format("%d %b");
 
 var xScale = d3.time.scale()  //Change data to pixel
     .range([0, width]);  //Pixel range
@@ -20,7 +25,7 @@ var yScale = d3.scale.linear()
 var xAxis = d3.svg.axis()
     .scale(xScale)
     .orient("bottom")
-    .tickFormat(date_format_tick);
+    .tickFormat(formatDate2);
 
 var yAxis = d3.svg.axis()
     .scale(yScale)
@@ -30,7 +35,7 @@ var line = d3.svg.line()
     .x(function(d) { return xScale(d[xCol]); })
     .y(function(d) { return yScale(d[yCol]); });
 
-//var tooltip = d3.select('body').append('div').style('position', 'absolute').style('padding', '0 10px').style('background', 'white').style('opacity', 0)
+var tooltip = d3.select('#SvgChart').append('div').style('position', 'absolute').style('padding', '0 10px').style('background', 'white').style('opacity', 0)
 
 var svg = d3.select("body").append("svg")
             .attr("width", width + margin.left + margin.right)
@@ -41,47 +46,13 @@ var g1= svg.append("g")
 
 function render(data) {
     
-//    var i = 0;
-//    for (i = 0; i < data.length; i++) { 
-//        if(data[i][yCol] == 0) {
-//            console.log( data[i][xCol]  );
-//            console.log( data[i][yCol]  );
-//            data.splice(i, 1);
-//        }
-//        
-//    }
         
     //set input data of scales
-    var minmaxDate = d3.extent(data, function(d) {return d[xCol]; })
-    
-    xScale.domain(minmaxDate);  //use "extent" to generate an array of min and max
-    yScale.domain(d3.extent(data, function(d) { return d[yCol]; }));
-    
-    var dateList = d3.time.day.range(minmaxDate[0], minmaxDate[1]);
-    console.log(dateList);
-    
-    //xScale.domain(dateList);
-//    
-//    xAxis.tickValues(d3.time.days(minmaxDate[0], minmaxDate[1])
-//                     .filter(function(d,i){
-//                                    console.log(data[i][xCol] );
-//                                    console.log(data[i][yCol] );
-//                                    console.log(data[i][xCol].getDay() );
-//                                    //return (data[i][xCol].getDay() !== 0);
-//                                    return false;
-//                                }) )
-//    
-//    ( xScale.domain()
-//                     .filter( function() {   return true; 
-//                    }))
-//    
-    g1.selectAll('rect').data(data).enter().append('rect')
-        .style('fill', "yellow" )
-        .attr('width', 5)
-        .attr('height', height)
-        .attr('x', function(data, i) { return xScale(data[xCol]); })
-        .attr('y', 0)
-            
+    var minmaxDate = d3.extent(data, function(d) {return d[xCol]; });   //use "extent" to generate an array of min and max
+    xScale.domain(minmaxDate);  
+    var minmaxVal = d3.extent(data, function(d) {return d[yCol]; });
+    yScale.domain(minmaxVal);
+                    
     //set x axis
     g1.append("g")
       .attr("class", "x axis")
@@ -90,7 +61,7 @@ function render(data) {
     // rotate the label of xaxis
     g1.selectAll(".x .tick text")  
         .attr("transform", function(d) {
-            return "translate(" + this.getBBox().height*-2 + "," + this.getBBox().height + ")rotate(-45)";
+            return "translate(" + this.getBBox().height*-2 + "," + (this.getBBox().height + 10) + ")rotate(-45)";
         });
 
     //set y axis
@@ -98,15 +69,28 @@ function render(data) {
       .attr("class", "y axis")
       .call(yAxis)
 
-//    svg.on('mouseover', function() {
-//           //tmpColor = this.style.fill;
-//           tooltip.transition().style('opacity', .9); 
-//           tooltip.html(data[xCol])
-//               .style('left',(d3.event.pageX) + 'px')
-//               .style('top', (d3.event.pageY) + 'px'); 
-//           d3.select(this).style('opacity', 0.5).style('fill', 'yellow'); 
-//            console.log("mouseover")
-//    })
+    var rect1 =  g1.selectAll('rect').data(data).enter().append('rect')
+        .style('fill', "white" )
+        .style('opacity', .1)
+        .attr('width', function(d, i) { return ( width/ (data.length) ) ; })
+        .attr('height', height)
+        .attr('x', function(d, i) { return (xScale(d[xCol]) -  width/ (data.length)/2) ; })
+        .attr('y', 0)
+    
+    rect1.on('mouseover', function(d) {
+           d3.select(this).style('opacity', 1).style('fill', "yellow");
+           tooltip.transition().style('opacity', .9); 
+           tooltip.html(d[yCol] + "<br>" + formatDate2(d[xCol])  )  //set the indication bar to the center of the xaxis tick
+               .style('left',(d3.event.pageX + 8) + 'px')
+               .style('top', (d3.event.pageY + 8) + 'px'); 
+           d3.select(this).style('opacity', 0.5).style('fill', 'yellow'); 
+            console.log("mouseover")
+    })
+    
+    rect1.on('mouseout', function() { 
+           d3.select(this).style('opacity', 0.1).style('fill', "white"); });
+
+    
     
     //draw the line    
     var path1 = g1.append("path");
@@ -123,7 +107,7 @@ function render(data) {
         });
 }
 
-//preprocess function for the data
+//preprocess the data
 function type(d) {
     if(formatDate1.parse(d[xCol]) != null)
         d[xCol] = formatDate1.parse(d[xCol]);
@@ -140,8 +124,7 @@ function type(d) {
 }
 
 d3.csv("QUALCOMM.csv", type, function(error, data) {
-  if (error) throw error;
-    
+  if (error) throw error;    
   render(data);
 });
 
