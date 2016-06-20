5993ce12a799a25f733af4698e19f8d855725f77
livelybug <livelybug@gmail.com>
2016-06-20 09:47:02 +0800

    To change the date scale to ordinal to filter some date

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ var margin = {top: 20, right: 20, bottom: 50, left: 50},
     height = 500 - margin.top - margin.bottom;
 
 var xCol = "date"
-var yCol = "pVolume"
+var yCol = "pOpen"
 
 var formatDate1 = d3.time.format("%d-%m-%Y");
 var formatDate2 = d3.time.format("%d/%m/%Y");
@@ -30,6 +30,8 @@ var line = d3.svg.line()
     .x(function(d) { return xScale(d[xCol]); })
     .y(function(d) { return yScale(d[yCol]); });
 
+//var tooltip = d3.select('body').append('div').style('position', 'absolute').style('padding', '0 10px').style('background', 'white').style('opacity', 0)
+
 var svg = d3.select("body").append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
@@ -39,17 +41,54 @@ var g1= svg.append("g")
 
 function render(data) {
     
-    //set input data of scales
-    xScale.domain(d3.extent(data, function(d) { return d[xCol]; }));  //use "extent" to generate an array of min and max
-    yScale.domain(d3.extent(data, function(d) { return d[yCol]; }));
+//    var i = 0;
+//    for (i = 0; i < data.length; i++) { 
+//        if(data[i][yCol] == 0) {
+//            console.log( data[i][xCol]  );
+//            console.log( data[i][yCol]  );
+//            data.splice(i, 1);
+//        }
+//        
+//    }
         
+    //set input data of scales
+    var minmaxDate = d3.extent(data, function(d) {return d[xCol]; })
+    
+    xScale.domain(minmaxDate);  //use "extent" to generate an array of min and max
+    yScale.domain(d3.extent(data, function(d) { return d[yCol]; }));
+    
+    var dateList = d3.time.day.range(minmaxDate[0], minmaxDate[1]);
+    console.log(dateList);
+    
+    //xScale.domain(dateList);
+//    
+//    xAxis.tickValues(d3.time.days(minmaxDate[0], minmaxDate[1])
+//                     .filter(function(d,i){
+//                                    console.log(data[i][xCol] );
+//                                    console.log(data[i][yCol] );
+//                                    console.log(data[i][xCol].getDay() );
+//                                    //return (data[i][xCol].getDay() !== 0);
+//                                    return false;
+//                                }) )
+//    
+//    ( xScale.domain()
+//                     .filter( function() {   return true; 
+//                    }))
+//    
+    g1.selectAll('rect').data(data).enter().append('rect')
+        .style('fill', "yellow" )
+        .attr('width', 5)
+        .attr('height', height)
+        .attr('x', function(data, i) { return xScale(data[xCol]); })
+        .attr('y', 0)
+            
     //set x axis
     g1.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis);
-    
-    g1.selectAll(".x .tick text")  // select all the text elements for the xaxis
+    // rotate the label of xaxis
+    g1.selectAll(".x .tick text")  
         .attr("transform", function(d) {
             return "translate(" + this.getBBox().height*-2 + "," + this.getBBox().height + ")rotate(-45)";
         });
@@ -59,6 +98,16 @@ function render(data) {
       .attr("class", "y axis")
       .call(yAxis)
 
+//    svg.on('mouseover', function() {
+//           //tmpColor = this.style.fill;
+//           tooltip.transition().style('opacity', .9); 
+//           tooltip.html(data[xCol])
+//               .style('left',(d3.event.pageX) + 'px')
+//               .style('top', (d3.event.pageY) + 'px'); 
+//           d3.select(this).style('opacity', 0.5).style('fill', 'yellow'); 
+//            console.log("mouseover")
+//    })
+    
     //draw the line    
     var path1 = g1.append("path");
     path1.attr("d", line(data))     
@@ -87,7 +136,7 @@ function type(d) {
     if(d[yCol] == 0)
         return null;
     
-    return d;
+        return d;
 }
 
 d3.csv("QUALCOMM.csv", type, function(error, data) {
